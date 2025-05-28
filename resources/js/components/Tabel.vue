2b13f1a138e8cd2065cc66ad3a671aaa94202ef2
Daffawijaya <daffayanwijaya@gmail.com>
2025-05-28 13:07:14 +0700

    add: table and daftar pasien

---
+++ resources/js/components/Tabel.vue
@@ -0,0 +1,39 @@
+<template>
+  <table border="1" cellpadding="8" cellspacing="0">
+    <thead>
+      <tr>
+        <th v-for="header in headers" :key="header">{{ header }}</th>
+        <th v-if="actions.length">Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in items" :key="index">
+        <td v-for="key in keys" :key="key">{{ item[key] }}</td>
+        <td v-if="actions.length">
+          <button
+            v-for="action in actions"
+            :key="action.name"
+            @click="$emit(action.event, item)"
+            style="margin-right: 4px"
+          >
+            {{ action.label }}
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    headers: Array,      // ['Nama', 'Umur', 'Jenis Kelamin']
+    keys: Array,         // ['nama', 'umur', 'jenis_kelamin']
+    items: Array,        // list data
+    actions: {           // opsional: tombol-tombol aksi
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
--- resources/js/view/DaftarPasien.vue
+++ resources/js/view/DaftarPasien.vue
@@ -1,2 +1,42 @@
-<template><h2>Daftar Pasien</h2><p>Ini halaman Daftar Pasien.</p></template>
-<script>export default { name: 'DaftarPasien' };</script>
+<template>
+  <div>
+    <h1>Daftar Pasien</h1>
+    <Tabel
+      :headers="['Nama', 'Umur', 'Jenis Kelamin', 'Poli Asal']"
+      :keys="['nama', 'umur', 'jenis_kelamin', 'poli_asal']"
+      :items="pasienList"
+      :actions="[{ name: 'detail', label: 'Detail', event: 'lihatDetail' }]"
+      @lihatDetail="tampilkanDetail"
+    />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Tabel from '../components/Tabel.vue'
+
+export default {
+  components: { Tabel },
+  data() {
+    return {
+      pasienList: []
+    }
+  },
+  mounted() {
+    this.ambilPasien()
+  },
+  methods: {
+    async ambilPasien() {
+      try {
+        const response = await axios.get('/api/pasien')
+        this.pasienList = response.data
+      } catch (error) {
+        console.error('Gagal ambil data pasien:', error)
+      }
+    },
+    tampilkanDetail(pasien) {
+      alert(`Detail pasien: ${pasien.nama}, umur ${pasien.umur}`)
+    }
+  }
+}
+</script>
