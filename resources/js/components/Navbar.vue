393cfdfec77674a9ad4a0424418ef7fb998160d6
Daffawijaya <daffayanwijaya@gmail.com>
2025-06-09 16:46:27 +0700

    add and fix feature tailwind instal poppins

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -2,9 +2,10 @@
     <div class="p-4 flex justify-end">
     <div class="relative"  @click="toggleDropdown" style="cursor: pointer; ">
      <img src="http://localhost:8000/image/default-profile.jpg" alt="Foto Profil" class="w-10 h-10 rounded-full" />
-    <div class="absolute right-0 mt-2 bg-white rounded-full shadow-md px-3 py-1" v-if="showDropdown " >
-      <button @click="logout" style="cursor: pointer;">
+    <div class="absolute right-0 mt-2 bg-white rounded-full shadow-md px-4 py-1.5" v-if="showDropdown " >
+      <button class="flex gap-x-1 items-center text-sm" @click="logout" style="cursor: pointer;">
         Keluar
+        <ArrowRightStartOnRectangleIcon class="size-5" />
       </button>
     </div>
   </div>
@@ -13,7 +14,12 @@
 </template>
 
 <script>
+import { ArrowRightStartOnRectangleIcon } from '@heroicons/vue/24/outline'
+
 export default {
+    components: {
+     ArrowRightStartOnRectangleIcon
+  },
   data() {
     return {
       showDropdown: false
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Vue + Laravel</title>
+<!-- Tambahkan ini di <head> -->
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+<title>Vue + Laravel</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
 </head>
-<body>
+<body> 
+    
     <div id="app"></div>
 </body>
 </html>
