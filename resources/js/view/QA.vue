6ee59212b018ea82ad57406c1d7a0f66999fd5ff
Daffawijaya <daffayanwijaya@gmail.com>
2025-06-09 12:28:17 +0700

    install tailwind

--- resources/js/view/QA.vue
+++ resources/js/view/QA.vue
@@ -1,2 +1,5 @@
-<template><h2>Q&A</h2><p>Ini halaman Q&A.</p></template>
+<template><h2>Q&A</h2><p>Ini halaman Q&A.</p>
+<div class="p-6 bg-blue-500 text-white font-bold rounded">
+    Tailwind CSS berhasil dipasang!
+  </div></template>
 <script>export default { name: 'QA' };</script>
