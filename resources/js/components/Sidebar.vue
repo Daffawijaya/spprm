9bbd40cdc719a5ce0c5dbdd77aff054bfb3e57ed
Daffawijaya <daffayanwijaya@gmail.com>
2025-06-19 14:26:08 +0700

    add parikesit lgo sidebar

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,6 +1,11 @@
 <template>
-  <nav class="bg-[#4047F3] w-48 min-h-screen pl-3 pt-8 text-white">
-    <ul>
+
+  <nav class="bg-[#4047F3] w-48 min-h-screen  pt-8 text-white space-y-22">
+    <div class="flex flex-row items-center gap-x-3 justify-center">
+      <img src="http://localhost:8000/image/parikesit.png" alt="Foto Profil" class="w-10 h-10" />
+      <p class="font-black text-lg">Parikesit</p>
+    </div>
+    <ul class="pl-3">
       <li v-for="item in menuItems" :key="item.to" class="relative py-3 text-sm"
         :class="{ 'with-right-bump': isActiveRoute(item.to) }">
         <router-link :to="item.to" :class="{
@@ -60,6 +65,7 @@ function isActiveRoute(path) {
   /* box shadow to give the shadow of the pseudo-element the same color as the background */
   box-shadow: 0 20px 0 0 #EFF2F9;
 }
+
 .with-right-bump :before {
   content: "";
   position: absolute;
