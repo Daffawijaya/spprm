94774023f42de5c61be95b4235294175b747069c
Daffawijaya <daffayanwijaya@gmail.com>
2025-06-20 15:40:25 +0700

    sidebar fix

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="flex bg-[#EFF2F9]">
-    <Sidebar />
+    <!-- Sticky Sidebar -->
+    <div class="fixed top-0 left-0 h-screen w-48 z-50">
+      <Sidebar />
+    </div>
 
-    <div class="p-7 flex-1 space-y-2">
+    <!-- Konten Utama -->
+    <div class="ml-48 p-7 flex-1 space-y-2 min-h-screen">
       <Navbar />
       <Breadcrumb />
       <router-view />
     </div>
   </div>
 </template>
+
 
 <script>
 import Sidebar from './components/Sidebar.vue';
@@ -20,4 +25,3 @@ export default {
   components: { Sidebar, Navbar, Breadcrumb }
 };
 </script>
-
